acad3f7bba6b1d964d7ab6eab1e5ae79aed9b4d8
yukiiyong <911711448@qq.com>
2021-08-16 01:53:07 +0800

    first

---
+++ dist/js/base.js
@@ -0,0 +1,5 @@
+let _mapVar = _map
+
+console.log('base-js')
+//测试provideplugin
+console.log(_mapVar)
---
+++ src/webpack-merge.js
@@ -0,0 +1,55 @@
+//示例，一般定义在webpack.config.js
+const merge = require('webpack-merge')
+
+merge({
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {a: 1}
+    ]
+  },
+  plugins: [2,3,4]
+}, {
+  devtool: 'none',
+  mode: 'production',
+  module: {
+    rules: [
+      {a: 2},
+      {b: 1}
+    ]
+  },
+  plugins: [6, 7, 8]
+});
+
+//合并后结果为
+{
+  devtool: 'none',
+  mode: 'production',
+  module: {
+    rules: [
+      {a: 1},
+      {a: 2},
+      {b: 1}
+    ]
+  },
+  plugins: [2, 3, 4, 6, 7, 8]
+}
+
+//也可以这样配置
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.config.base')
+
+module.exports = merge(baseWebpackConfig, {
+  mode: 'development'
+  //其他一些配置
+})
+
+//修改package.json中对应的config文件
+
+//package.json
+{
+  "scripts": {
+    "dev": "cross-env NODE_ENV=development webpack-dev-server --config=webpack.config.dev.js",
+    "build": "cross-env NODE_ENV=production webpack --config=webpack.config.prod.js"
+  }
+}
